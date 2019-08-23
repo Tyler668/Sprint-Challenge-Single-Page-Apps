bdb462e7ff8d36477aed02ebd5ca3ee11ecb0c3f
Tyler Gilchrist <tyler668@gmail.com>
2019-08-23 12:12:03 -0400

    structured location cards and added files for episodes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
     <main>
       <Header />
       {/* <CharacterList/> */}
-      <LocationsList/>
+      {/* <LocationsList/> */}
       {/* <TabNav /> */}
     </main>
   );
--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -3,8 +3,7 @@ import React from "react";
 export default function LocationCard(props) {
   return <div className = "charCard">
   <h2>{props.info.name}</h2>
-  <h3>Dimension: {props.info.dimension}</h3>
-  <h4>Classification: {props.info.type}</h4>
-  <h4>Residents: {}</h4>
+  <h4>{props.info.type}-{props.info.dimension}</h4>
+  <h4>{props.info.residents.length} Residents</h4>
 </div>;
 }
