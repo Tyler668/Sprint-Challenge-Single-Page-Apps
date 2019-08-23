6012cc54ddad4567ba7bb94c0058d1582f31e6a8
Tyler Gilchrist <tyler668@gmail.com>
2019-08-23 13:06:22 -0400

    routes are rigged

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import LocationsList from "./components/LocationsList";
 import { logValidationWarning } from "jest-validate/build/utils";
 import EpisodeList from "./components/EpisodeList";
 import styled from 'styled-components';
+import { Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import WelcomePage from './components/WelcomePage'
 
 const Main = styled.main`
 background: #1b5c47;
@@ -16,12 +19,19 @@ background: #1b5c47;
 
 export default function App() {
   return (
-    <Main>
-      <Header />'
-      {/* <EpisodeList/> */}
-      {/* <CharacterList/> */}
-      {/* <LocationsList/> */}
-      {/* <TabNav /> */}
-    </Main>
+    <Router>
+      <Main>
+        <Header />'
+      <Route exact path="/" render={props => <WelcomePage {...props} />} />
+      <Route  path="/characters" render={props => <CharacterList {...props} characters={CharacterList}/>} />
+      <Route  path="/locations" render={props => <LocationsList {...props} locations={LocationsList}/>} />
+      <Route  path="/episodes" render={props => <EpisodeList {...props} episodes={EpisodeList}/>} />
+        {/* <EpisodeList/> */}
+        {/* <CharacterList/> */}
+        {/* <LocationsList/> */}
+        {/* <TabNav /> */}
+      </Main>
+    </Router>
+
   );
 }
